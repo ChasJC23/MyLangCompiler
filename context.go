14230d03db06d2d74e253546c4354bc71928f501
ChasJC23 <45319866+ChasJC23@users.noreply.github.com>
2022-02-03 23:18:49 +0000

    populate context

--- context.go
+++ context.go
@@ -3,12 +3,51 @@ package main
 import "container/list"
 
 type OperatorTree struct {
-	branches      map[rune]OperatorTree
+	branches      map[rune]*OperatorTree
 	childOpCount  int
 	operatorToken int
 }
 
+func NewOperatorTree() *OperatorTree {
+	m := make(map[rune]*OperatorTree)
+	o := new(OperatorTree)
+	o.branches = m
+	o.childOpCount = 0
+	o.operatorToken = -1
+	return o
+}
+
+func (tree *OperatorTree) AddOperator(ra []rune, token int) bool {
+	if len(ra) == 0 {
+		if tree.operatorToken == -1 {
+			tree.operatorToken = token
+			return true
+		} else {
+			return false
+		}
+	}
+	c := ra[0]
+	branch, ok := tree.branches[c]
+	if !ok {
+		branch = NewOperatorTree()
+		tree.branches[c] = branch
+	}
+	success := branch.AddOperator(ra[1:], token)
+	if success {
+		tree.childOpCount++
+	}
+	return success
+}
+
 func (tree *OperatorTree) PossibleCount(ra []rune) (int, *OperatorTree) {
+	count, subtree := tree.PossibleChildCount(ra)
+	if subtree.operatorToken != -1 {
+		count++
+	}
+	return count, subtree
+}
+
+func (tree *OperatorTree) PossibleChildCount(ra []rune) (int, *OperatorTree) {
 	if len(ra) == 0 {
 		if tree.operatorToken != -1 {
 			return 1, tree
@@ -18,7 +57,7 @@ func (tree *OperatorTree) PossibleCount(ra []rune) (int, *OperatorTree) {
 	c := ra[0]
 	branch, ok := tree.branches[c]
 	if ok {
-		return branch.PossibleCount(ra[1:])
+		return branch.PossibleChildCount(ra[1:])
 	} else {
 		return 0, tree
 	}
@@ -37,16 +76,45 @@ func (tree *OperatorTree) OperatorExists(ra []rune) bool {
 }
 
 type OpContext struct {
-	levels list.List
+	levels  *list.List
+	opToken int
+}
+
+func NewOpContext() *OpContext {
+	l := list.New()
+	t := 10
+	r := new(OpContext)
+	r.levels = l
+	r.opToken = t
+	return r
+}
+
+func (ctx *OpContext) AddOperator(op []rune, precedence int) bool {
+	level := ctx.levels.Front()
+	for i := 0; i < precedence; i++ {
+		if level == nil {
+			level = ctx.levels.PushBack(NewOperatorTree())
+		}
+		level = level.Next()
+	}
+	if level == nil {
+		level = ctx.levels.PushBack(NewOperatorTree())
+	}
+	tree := level.Value.(*OperatorTree)
+	success := tree.AddOperator(op, ctx.opToken)
+	if success {
+		ctx.opToken++
+	}
+	return success
 }
 
 func (ctx *OpContext) PossibleCount(ra []rune) int {
 	element := ctx.levels.Front()
-	tree := element.Value.(OperatorTree)
+	tree := element.Value.(*OperatorTree)
 	total, _ := tree.PossibleCount(ra)
 	for i := 1; i < ctx.levels.Len(); i++ {
 		element = element.Next()
-		tree := element.Value.(OperatorTree)
+		tree := element.Value.(*OperatorTree)
 		inc, _ := tree.PossibleCount(ra)
 		total += inc
 	}
@@ -55,11 +123,11 @@ func (ctx *OpContext) PossibleCount(ra []rune) int {
 
 func (ctx *OpContext) OperatorExists(ra []rune) bool {
 	element := ctx.levels.Front()
-	tree := element.Value.(OperatorTree)
+	tree := element.Value.(*OperatorTree)
 	exists := tree.OperatorExists(ra)
 	for !exists {
 		element = element.Next()
-		tree := element.Value.(OperatorTree)
+		tree := element.Value.(*OperatorTree)
 		exists = exists || tree.OperatorExists(ra)
 	}
 	return exists
--- main.go
+++ main.go
@@ -3,26 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/fs"
-	"os"
+	// "io/fs"
+	// "os"
 )
 
-var srcFilePath = flag.Arg(0)
+// var srcFilePath = flag.Arg(0)
 
 func init() {
 	flag.Parse()
-	if !fs.ValidPath(srcFilePath) {
-		fmt.Println("Path does not point to a valid source file.")
-		os.Exit(2)
-	}
+	// if !fs.ValidPath(srcFilePath) {
+	// 	fmt.Println("Path does not point to a valid source file.")
+	// 	os.Exit(2)
+	// }
 }
 
 func main() {
-	srcFile, err := os.Open(srcFilePath)
-	if err != nil {
-		panic(err)
-	}
-	defer srcFile.Close()
+	// srcFile, err := os.Open(srcFilePath)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer srcFile.Close()
+
+	ctx := NewOpContext()
+	ctx.AddOperator([]rune("++"), 0)
+	ctx.AddOperator([]rune("+"), 0)
+	ctx.AddOperator([]rune("-"), 0)
+	ctx.AddOperator([]rune("--"), 0)
+
+	fmt.Println(ctx.PossibleCount([]rune("")))
 
 	// using runes
 	// srcReader := bufio.NewReader(srcFile)
