32ceba581f3189542bf742c8270d315035b11b29
ChasJC23 <45319866+ChasJC23@users.noreply.github.com>
2022-02-09 05:17:12 +0000

    that's a lot of functions

--- parser.go
+++ parser.go
@@ -50,19 +50,44 @@ func (p *Parser) ParsePrecisionLevel(preclvlel *list.Element) interface{} {
 	// check bitmask in preclevel.go
 	switch preclvl.properties & 0b1111 {
 	case 0b0000: // prefix
+		p.ParsePrefix(preclvl)
 	case 0b0001: // postfix
+		p.ParsePostfix(preclvl)
 	case 0b0010: // infix left associative
+		p.ParseLeftAssociative(preclvl)
 	case 0b0011: // infix right associative
+		p.ParseRightAssociative(preclvl)
 	case 0b0100: // repeatable prefix
+		p.ParseRepeatablePrefix(preclvl)
 	case 0b0101: // repeatable postfix
+		p.ParseRepeatablePostfix(preclvl)
 	case 0b0110: // repeatable infix left associative
+		p.ParseRepeatableLeftAssociative(preclvl)
 	case 0b0111: // repeatable infix right associative
+		p.ParseRepeatableRightAssociative(preclvl)
 	case 0b1010: // implied operation infix left associative
+		p.ParseImpliedLeftAssociative(preclvl)
 	case 0b1011: // implied operation infix right associative
+		p.ParseImpliedRightAssociative(preclvl)
 	case 0b1110: // implied operation repeatable infix left associative
+		p.ParseImpliedRepeatableLeftAssociative(preclvl)
 	case 0b1111: // implied operation repeatable infix right associative
+		p.ParseImpliedRepeatableRightAssociative(preclvl)
 	default:
 		panic("invalid configuration")
 	}
 	return nil
 }
+
+func (p *Parser) ParseImpliedLeftAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParseImpliedRepeatableLeftAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParseImpliedRightAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParseImpliedRepeatableRightAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParseLeftAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParseRepeatableLeftAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParseRightAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParseRepeatableRightAssociative(preclvl *PrecedenceLevel)
+func (p *Parser) ParsePrefix(preclvl *PrecedenceLevel)
+func (p *Parser) ParseRepeatablePrefix(preclvl *PrecedenceLevel)
+func (p *Parser) ParsePostfix(preclvl *PrecedenceLevel)
+func (p *Parser) ParseRepeatablePostfix(preclvl *PrecedenceLevel)
